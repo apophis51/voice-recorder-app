1b8b87f26bea1992564684a4b255ed4e399d3334
apophis51 <malcolmxvernon@hotmail.com>
2025-02-09 00:16:34 -0500

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.malcmind.app',
+  appName: 'MalcMind AI Note Taker',
+  webDir: 'out'
+};
+
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,48 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
 
-export default nextConfig;
+const nextConfig = {
+  experimental: {
+    // swcPlugins: [["@preact-signals/safe-react/swc", {mode: "auto",
+    // }]],
+    reactCompiler: true,
+    // ppr: 'incremental',
+
+  },
+  
+
+
+  // experimental: {
+  //   serverActions: true,
+  // },
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat',
+  //       "react/jsx-runtime": "preact/jsx-runtime"
+  //     })
+  //   }
+
+  //   return config
+  // },
+  typescript: {            //added 2023 to prevent typescript build errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+		unoptimized: true
+	},
+  output: 'export',
+
+  
+  
+  reactStrictMode: false,
+  
+  
+}
+
+module.exports = nextConfig
